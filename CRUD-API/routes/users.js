d74cbc680ecd062c8a60c4a2e00601fb2027b3f9
Goutham M <gouthamgo@yahoo.com>
2024-06-01 21:29:12 +1000

    uuid route

--- CRUD-API/routes/users.js
+++ CRUD-API/routes/users.js
@@ -1,20 +1,24 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 // Router initialization
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 25
-    },
-    {
-        firstName: "kris",
-        lastName: "rock",
-        age: 22
-    }
-];
+const users =[];
+
+// const users = [
+//     {
+//         firstName: "John",
+//         lastName: "Doe",
+//         age: 25
+//     },
+//     {
+//         firstName: "kris",
+//         lastName: "rock",
+//         age: 22
+//     }
+// ];
 
 // GET route for /users
 router.get('/', (req, res) => {
@@ -28,9 +32,22 @@ router.post('/', (req,res)=>{
 
     const user = req.body;
 
-    users.push(user);
+    const userId= uuidv4(); 
+
+    const userWithId = {...user, id:userId};
+
+    users.push(userWithId);
 
     res.send('POST ROUTE REACHED')
 });
+
+router.get('/:id', (req, res)=>{
+    console.log(req.params)
+    const {id} = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+
+    res.send(foundUser);
+})
 
 export default router;
