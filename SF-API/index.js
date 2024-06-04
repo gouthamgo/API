add1aac4928fa300409ecd23f820e99ebcf7b08a
Goutham M <gouthamgo@yahoo.com>
2024-06-04 23:07:29 +1000

    SF-API- connected with sf org and pulled the records

--- SF-API/index.js
+++ SF-API/index.js
@@ -1,18 +1,42 @@
 import express from 'express';
 import 'dotenv/config'
 
-var jsforce = require('jsforce');
+ import jsforce from 'jsforce'
 
 const app = express();
 const PORT = 3001;
 
 
-const{SF_LOGIN}
+const{SF_LOGIN_URL ,SF_USERNAME ,SF_PASSWORD, SF_TOKEN} = process.env;
+
+var conn = new jsforce.Connection({
+    loginUrl:SF_LOGIN_URL
+});
+
+conn.login(SF_USERNAME, SF_PASSWORD+SF_TOKEN,(err,userInfo)=>{
+    if(err){
+        console.error(err);
+    }
+    else{
+        console.log("User Id"+ userInfo.id)
+        console.log("Org Id"+userInfo.organizationId)
+    }
+})
+
+
 
 
 
 app.get('/',(req,res)=>{
-    res.send("pass to salesforce")
+    conn.query("SELECT Id, Name FROM Account",(err,result)=>{
+        if(err){
+            res.send(err);
+        }
+        else{ 
+            console.log("Total Requests"+ result.totalSize);
+            res.json(result.records);
+        }
+    } )
 })
 
 app.listen(PORT, () => console.log(`The server is running at http://localhost:${PORT}`));
